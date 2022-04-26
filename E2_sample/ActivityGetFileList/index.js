d858f4adf48d05af922acf1c7dddf61c41bf2afa
okuyama <t_okuyama@ap-com.co.jp>
2022-04-26 09:07:01 +0900

    ファンアウト・ファンイン関数を追加

---
+++ E2_sample/ActivityGetFileList/index.js
@@ -0,0 +1,36 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+const readdirp = require("readdirp");
+
+//module.exports = async function (context, rootDir) {
+// async から変更
+module.exports = function (context, rootDir) {
+    context.log("Try to find files in rootDir :" + rootDir);
+    const allFilePaths = [];
+
+    readdirp(
+        { root: rootDir, entryType: 'all' },
+        function (fileInfo) {
+            if (!fileInfo.stat.isDirectory()){    
+                console.log("fileInfo.fullPath: " + fileInfo.fullPath);            
+                allFilePaths.push(fileInfo.fullPath);
+            }
+        },
+        function(err, res){
+            if (err) {
+                throw err;
+            }
+            
+            console.log(`Found ${allFilePaths.length} files in ${rootDir}`);
+            context.done(null, allFilePaths);            
+        }
+    );
+};
---
+++ E2_sample/MyOrchestrator/index.js
@@ -0,0 +1,44 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    console.log('start my orchestrator... ');
+
+    //パラメーターチェック body部分
+    const rootDir = context.df.getInput();
+    if ( !rootDir ) {
+        throw new Error("A dir should be.");        
+    }
+    console.log("rootDir: " + rootDir);
+
+    //activityの呼び出し
+    //rootDir配下のファイルの一覧
+    const files = yield context.df.callActivity("ActivityGetFileList", rootDir);
+    console.log("MyOrchestrator.files :" + files);
+
+    //Promiseの配列を作成
+    const tasks = [];
+    for (const file of files){
+        console.log(file);
+        tasks.push(context.df.callActivity("ActivityCopyFile2Blob",file));
+    }
+    //console.log(tasks);
+
+    const results = yield context.df.Task.all(tasks);
+    console.log('Task.all done.');
+
+    const totalBytes = results.reduce((prev, curr) => { return prev + curr }, 0);
+
+    console.log("totalBytes :" + totalBytes);
+    return totalBytes;
+});
